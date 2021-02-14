8e6ccbe9e410f57c703012b99c544591b2ba4bf3
Your Name <mustafahassan.dec@gmail.com>
2021-02-14 21:04:09 +0200

    Adding User Interface to The Game

--- script.js
+++ script.js
@@ -1,39 +1,96 @@
+let round = 0;
+let cScore = 0;
+let pScore = 0;
 function computerPlay() {
     let randomNumber = Math.ceil(Math.random() * 3);
+    let select = '';
     switch(randomNumber) {
         case 1:
-            return 'Rock';
+            select = 'Rock';
+            break;
         case 2:
-            return 'Paper';
+            select = 'Scissors';
+            break;
         case 3:
-            return 'Scissors';
+            select = 'Paper';
+            break;
     }
+    return select;
 }
 
 function playRound(playerSelection, computerSelection) {
-    ps = playerSelection.toLowerCase();
-    cs = computerSelection.toLowerCase();
-    if(cs === ps) 
-        return 'Tie game!'
-    else if(cs == 'rock' && ps == 'scissors') 
-        return `You Lose! ${cs} beats ${ps}`;
-    else if (cs == 'scissors' && ps == 'paper')
-        return `You Lose! ${cs} beats ${ps}`;
-    else if (cs == 'paper' && ps == 'rock') 
-        return `You Lose! ${cs} beats ${ps}`;
-    
-    else 
-        return `You Win! ${ps} beats ${cs}`;
-}
-
-function game() {
-    let round = 1;
-    while(round <= 5) {
-        const computerSelection = computerPlay();
-        const playerSelection = prompt('Enter a selection');
-        console.log(playRound(playerSelection, computerSelection));
-        round++;
+    computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    let youWin = true;
+    let result = ''
+    if(computerSelection === playerSelection) {
+        return 'Tie Game';
     }
+    else if(computerSelection == 'rock' && playerSelection == 'scissors') {
+        youWin = false;
+    } else if(computerSelection == 'scissors' && playerSelection == 'paper') {
+        youWin = false;
+    } else if(computerSelection == 'paper' && playerSelection == 'rock') {
+        youWin = false;
+    }
+    computerSelection = computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1);
+    playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1);
+    if(youWin) {
+        pScore++;
+        result =   `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        cScore++;
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    return result;
 }
 
-game();
+
+const roundDiv = document.querySelector('.round');
+const roundNumber = document.createElement('span');
+const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
+const text = document.querySelector('.text');
+const winer = document.querySelector('.winer');
+const computerScore = document.querySelector('.computer-score');
+const playerScore = document.querySelector('.player-score');
+const computerScoreSpan = document.createElement('span');
+const playerScoreSpan = document.createElement('span');
+
+roundNumber.textContent = round;
+roundDiv.appendChild(roundNumber);
+computerScoreSpan.textContent = cScore;
+playerScoreSpan.textContent = pScore;
+computerScore.appendChild(computerScoreSpan);
+playerScore.appendChild(playerScoreSpan);
+
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        winer.textContent = '';
+        winer.style.backgroundColor = '#181818';
+        round++;
+        roundNumber.textContent = round;
+        roundDiv.appendChild(roundNumber);
+        const playerSelection = button.id;
+        const computerSelection = computerPlay();
+        text.textContent = playRound(playerSelection, computerSelection);
+        computerScoreSpan.textContent = cScore;
+        playerScoreSpan.textContent = pScore;
+        computerScore.appendChild(computerScoreSpan);
+        playerScore.appendChild(playerScoreSpan);
+        if(cScore == 5){
+            winer.style.backgroundColor = 'red';
+            winer.textContent = 'The Winer is: Computer';
+            round = cScore = pScore = 0;
+        }
+        else if(pScore == 5) {
+        winer.style.backgroundColor = 'red';
+        winer.textContent = 'The Winer is: Human';
+        round = cScore = pScore = 0;
+        }
+    });
+});
+
+
