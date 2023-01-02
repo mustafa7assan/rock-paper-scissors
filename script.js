5a3ab52d5a491e3b068508f15adc7b64de069bfd
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-01-02 21:53:45 +0200

    Add playRound function PlayRound function take player selection and computer selection to determine the winner

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ function getComputerChoice() {
       return "scissors";
   }
 }
+
+function capitalize(s) {
+  return s.replace(s[0], s[0].toUpperCase());
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let win = true;
+  if (computerSelection === playerSelection) {
+    return "Tie";
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    win = false;
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    win = false;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    win = false;
+  }
+  if (win) {
+    return `You win! ${capitalize(playerSelection)} beats ${capitalize(
+      computerSelection
+    )}`;
+  } else {
+    return `You lose! ${capitalize(computerSelection)} beats ${capitalize(
+      playerSelection
+    )}`;
+  }
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerSelection));
