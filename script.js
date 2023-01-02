257220d085bcfd9fe84c83c244be16e60bb3a76c
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-01-02 21:04:43 +0200

    Enable the computer to make a choice getComputerChoice generate a random number and chose a choice based on that number

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("It's works");
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3) + 1;
+  switch (choice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+}
