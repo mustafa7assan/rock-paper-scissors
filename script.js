6324e9affa19dfefbe62de7934a5a60e79ad023a
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-01-03 08:01:12 +0200

    Add game function Game function make 5 round and get choice from human player and computer at each iteration

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice() {
+  // generate a random number between 1 to 3 inclusive
   const choice = Math.floor(Math.random() * 3) + 1;
+  // based on random number select a choice
   switch (choice) {
     case 1:
       return "rock";
@@ -11,12 +13,15 @@ function getComputerChoice() {
 }
 
 function capitalize(s) {
+  // replace every first character to their upper case
   return s.replace(s[0], s[0].toUpperCase());
 }
 
 function playRound(playerSelection, computerSelection) {
+  // convert player selection to lower case
   playerSelection = playerSelection.toLowerCase();
   let win = true;
+  // determine the winner
   if (computerSelection === playerSelection) {
     return "Tie";
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
@@ -26,17 +31,40 @@ function playRound(playerSelection, computerSelection) {
   } else if (computerSelection === "paper" && playerSelection === "rock") {
     win = false;
   }
+  // based on winner increase the score of each player
   if (win) {
+    playerScore++;
     return `You win! ${capitalize(playerSelection)} beats ${capitalize(
       computerSelection
     )}`;
   } else {
+    computerScore++;
     return `You lose! ${capitalize(computerSelection)} beats ${capitalize(
       playerSelection
     )}`;
   }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  // play 5 round
+  for (let i = 1; i <= 5; i++) {
+    // get computer choice
+    const computerSelection = getComputerChoice();
+    // get player choice
+    let playerSelection = prompt("Enter your choice(rock, paper, scissors)");
+    while (!"(rock, paper, scissors)".includes(playerSelection.toLowerCase())) {
+      alert("Choose valid choice");
+      playerSelection = prompt("Enter your choice(rock, paper, scissors)");
+    }
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  // determine the winner
+  if (playerScore > computerScore) {
+    alert("You win");
+  } else {
+    alert("You lose");
+  }
+}
+let playerScore = 0,
+  computerScore = 0;
+game();
