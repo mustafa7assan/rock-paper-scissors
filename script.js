ede0de1db70130e7906265f3bbde195a360a9812
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-02-06 21:43:43 +0200

    Add ui

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-const buttons = document.querySelectorAll("button");
-const result = document.querySelector(".result");
-const pScore = document.querySelector(".player-score");
-const cScore = document.querySelector(".computer-score");
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const playSelection = btn.id;
-    const computerSelection = getComputerChoice();
-    result.textContent = playRound(playSelection, computerSelection);
-    pScore.textContent = playerScore;
-    cScore.textContent = computerScore;
-    checkWinner();
-  });
-});
-
-function checkWinner() {
-  if (playerScore >= 5) {
-    result.textContent = "Player is win";
-    rest();
-  } else if (computerScore >= 5) {
-    result.textContent = "Computer is win";
-    rest();
-  }
-}
-function rest() {
-  playerScore = 0;
-  computerScore = 0;
-  pScore.textContent = 0;
-  cScore.textContent = 0;
-}
 function getComputerChoice() {
   // generate a random number between 1 to 3 inclusive
   const choice = Math.floor(Math.random() * 3) + 1;
@@ -48,12 +18,16 @@ function capitalize(s) {
 }
 
 function playRound(playerSelection, computerSelection) {
+  // change hands
+  changeImage(playerSelection, computerSelection);
+  playerHand.classList.remove("winner");
+  computerHand.classList.remove("winner");
   // convert player selection to lower case
   playerSelection = playerSelection.toLowerCase();
   let win = true;
   // determine the winner
   if (computerSelection === playerSelection) {
-    return "Tie";
+    return "Tie Game";
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
     win = false;
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
@@ -64,16 +38,70 @@ function playRound(playerSelection, computerSelection) {
   // based on winner increase the score of each player
   if (win) {
     playerScore++;
+    pScore.textContent = playerScore;
+    playerHand.classList.add("winner");
     return `You win! ${capitalize(playerSelection)} beats ${capitalize(
       computerSelection
     )}`;
   } else {
     computerScore++;
-    return `You lose! ${capitalize(computerSelection)} beats ${capitalize(
+    cScore.textContent = computerScore;
+    computerHand.classList.add("winner");
+    return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(
       playerSelection
     )}`;
   }
 }
 
-let playerScore = 0,
+function changeImage(playerSelection, computerSelection) {
+  playerHand.src = `./img/${playerSelection}.png`;
+  playerHand.style.display = "block";
+  computerHand.src = `./img/${computerSelection}.png`;
+  computerHand.style.display = "block";
+}
+function checkWinner() {
+  if (playerScore >= 5) {
+    modal.style.display = "block";
+    modalText.textContent = "Player Win";
+    container.classList.add("new");
+  } else if (computerScore >= 5) {
+    modal.style.display = "block";
+    modalText.textContent = "Computer Win";
+    container.classList.add("new");
+  }
+}
+
+function reset() {
+  modal.style.display = "none";
+  container.classList.remove("new");
+  playerScore = 0;
   computerScore = 0;
+  modalText.textContent = "";
+  result.textContent = "";
+  pScore.textContent = 0;
+  cScore.textContent = 0;
+  playerHand.style.display = "none";
+  computerHand.style.display = "none";
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const container = document.querySelector(".container");
+const buttons = document.querySelectorAll(".btns button");
+const playerHand = document.querySelector(".player-hand");
+const computerHand = document.querySelector(".computer-hand");
+const pScore = document.querySelector(".player-score");
+const cScore = document.querySelector(".computer-score");
+const result = document.querySelector(".result");
+const modal = document.querySelector(".modal");
+const modalText = document.querySelector(".modal p");
+const restButton = document.querySelector(".rest");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = button.id;
+    result.textContent = playRound(playerSelection, computerSelection);
+    checkWinner();
+  });
+});
+restButton.addEventListener("click", reset);
